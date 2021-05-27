6098b732756b8c906ee92832bf3b418c61520ea9
unitythemaker <unitythemaker+github@gmail.com>
2021-05-27 10:47:39 +0300

    switch to fastify from python

---
+++ index.js
@@ -0,0 +1,23 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({
+    logger: true,
+});
+
+fastify.register(require('fastify-static'), {
+    root: __dirname + '/static',
+    prefix: '/',
+  });
+
+// Declare a route
+fastify.get('/api', function (request, reply) {
+    reply.send({ hello: 'world' });
+});
+
+// Run the server!
+fastify.listen(process.env.PORT || 3000, function (err, address) {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+    fastify.log.info(`server listening on ${address}`);
+});
